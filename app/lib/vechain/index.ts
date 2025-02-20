2fc6223c1f1e67ff2619345c9f5beb3eb1700191
virtualflame <zbarnes021993@gmail.com>
2025-02-20 16:38:21 -0500

    Clean up Connex initialization and consolidate exports

--- app/lib/vechain/index.ts
+++ app/lib/vechain/index.ts
@@ -1,3 +1,10 @@
+// Export all vechain utilities
 export * from './connex'
 export * from './contracts'
-export * from './testnet' 
+export * from './testnet'
+
+// Export constants
+export const TESTNET_CONFIG = {
+  node: 'https://testnet.veblocks.net',
+  network: 'test'
+} as const 

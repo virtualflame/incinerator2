b6027e60f1c35a02a92ad2e73788b00a1a501656
virtualflame <zbarnes021993@gmail.com>
2025-02-24 11:39:14 -0500

    Rename NFTBurner to NFTRecycler to better reflect functionality

---
+++ contracts/NFTRecycler.sol
@@ -0,0 +1,10 @@
+/**
+ * @title NFTRecycler
+ * @dev Contract that allows users to recycle old NFTs in exchange for B3TR tokens
+ * - NFTs are transferred to a mother wallet (not burned)
+ * - Users receive B3TR tokens as rewards
+ * - Supports two active collections per week
+ */
+contract NFTRecycler is Ownable {
+    // ... rest of the contract stays the same ...
+} 

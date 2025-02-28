3b344a057b838e7832fa5ef84ea730b0f2f72d82
virtualflame <zbarnes021993@gmail.com>
2025-02-28 16:27:26 -0500

    Add: Test NFT contract with batch minting

--- contracts/TestNFTCollection.sol
+++ contracts/TestNFTCollection.sol
@@ -1,24 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TestNFTCollection is ERC721, Ownable {
-    uint256 public totalSupply = 100;
-    uint256 public mintedCount = 0;
+    uint256 private _nextTokenId;
 
     constructor(
-        string memory name, 
+        string memory name,
         string memory symbol,
         address initialOwner
     ) ERC721(name, symbol) Ownable(initialOwner) {}
-    
-    function batchMint(uint256 count) public {
-        require(mintedCount + count <= totalSupply, "Would exceed total supply");
-        for(uint256 i = 0; i < count; i++) {
-            _safeMint(msg.sender, mintedCount);
-            mintedCount++;
+
+    function mint() public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function batchMint(uint256 amount) public onlyOwner {
+        for(uint256 i = 0; i < amount; i++) {
+            mint();
         }
     }
 } 

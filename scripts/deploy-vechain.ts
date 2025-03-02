b41d0d080c3925190d57b932020515dabbd97ff6
virtualflame <zbarnes021993@gmail.com>
2025-03-01 23:44:46 -0500

    Fix: Update contract deployment to use waitForDeployment

--- scripts/deploy-vechain.ts
+++ scripts/deploy-vechain.ts
@@ -3,6 +3,6 @@ import { ethers } from 'hardhat'
 export async function deployContract(name: string, args: any[]) {
   const Contract = await ethers.getContractFactory(name)
   const contract = await Contract.deploy(...args)
-  await contract.deployed()
+  await contract.waitForDeployment()
   return contract
 } 

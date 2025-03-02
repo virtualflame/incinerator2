3bef6f50513327e3c53f4702df188eb9beb56937
virtualflame <zbarnes021993@gmail.com>
2025-03-01 23:02:12 -0500

    Remove: Clean up test NFT deployment code and types

--- app/lib/contracts/types.ts
+++ app/lib/contracts/types.ts
@@ -1,17 +1,4 @@
-export interface TestNFTContract {
-  mint(): Promise<any>
-  batchMint(count: number): Promise<any>
-  totalSupply(): Promise<number>
-  balanceOf(owner: string): Promise<number>
-  ownerOf(tokenId: number): Promise<string>
-}
-
-export interface NFTRecyclerContract {
-  updateCollection(
-    contractAddress: string,
-    rewardAmount: number,
-    isActive: boolean
-  ): Promise<any>
-  recycleNFT(collection: string, tokenId: number): Promise<any>
-  togglePause(): Promise<any>
+export interface ContractArtifact {
+  bytecode: string
+  abi: any[]
 } 
